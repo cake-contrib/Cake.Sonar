75b98149f0a9aa2aa4f4bb9277c8e2ab4f8118ca
phrusher <jenshorn@gmail.com>
2016-11-21 14:19:42 +0100

    Added SecretArgumentAttribute for sensitive configuration.

--- src/Cake.Sonar/Attributes/ArgumentAttribute.cs
+++ src/Cake.Sonar/Attributes/ArgumentAttribute.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Cake.Sonar
+namespace Cake.Sonar.Attributes
 {
     [AttributeUsage(AttributeTargets.Property)]
     public class ArgumentAttribute : Attribute 
